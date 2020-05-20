288b4a59e2864e07ff20b8ab81d33bd1e9c52e47
”espoiravenir465” <lapis074@gmail.com>
2020-05-20 18:42:44 +0200

    gitignorefix0520

--- pgAdmin 4.app/Contents/Resources/web/config_distro.py
+++
@@ -1,9 +0,0 @@
-SERVER_MODE = False
-MINIFY_HTML = False
-HELP_PATH = '../../../docs/en_US/html/'
-UPGRADE_CHECK_KEY = 'edb-pgadmin4'
-DEFAULT_BINARY_PATHS = {
-"pg":   "/Users/emi-h/schedule2/bin",
-"ppas": ""
-}
-                    
